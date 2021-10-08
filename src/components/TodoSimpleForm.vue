3b9818bb166c7344d2f35f91dc308a0c6ada2f18
AnJuHwan <wnghks0531@gmail.com>
2021-10-08 17:55:04 +0900

    파일분리 , emit , emits , props

--- src/components/TodoSimpleForm.vue
+++ src/components/TodoSimpleForm.vue
@@ -20,7 +20,36 @@
 </template>
 
 <script>
-export default {};
+import { ref } from '@vue/reactivity';
+export default {
+  emits: ['add-todo'],
+  setup(props, { emit }) {
+    // context : 자식 -> 부모 데이터 옮길때 context.emit 사용
+    const todo = ref('');
+    const hasError = ref(false);
+
+    const onSubmit = () => {
+      if (todo.value == '') {
+        hasError.value = true;
+      } else {
+        emit('add-todo', {
+          id: Date.now(),
+          subject: todo.value,
+          completed: false,
+        });
+
+        hasError.value = false;
+        todo.value = '';
+      }
+    };
+
+    return {
+      todo,
+      hasError,
+      onSubmit,
+    };
+  },
+};
 </script>
 
 <style></style>
